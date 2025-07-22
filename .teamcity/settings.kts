497943518afd4951697ba0c04bf5720674753f44
vasylshlapaks <vasil.shlapak@user1st.com>
2025-07-22 20:52:56 +0200

    Test

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,3 +1,10 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+
+version = "2025.03"
+
 project {
 
     buildType(Build)
